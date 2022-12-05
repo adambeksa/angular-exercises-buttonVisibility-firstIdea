f7d3ecf44151113bc353258234d1dad48685fb71
adambeksa <beksaadam@gmail.com>
2022-12-05 20:15:46 +0100

    Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { filter, Observable } from 'rxjs';
+import { addToCartVisibiltyService } from './app.service';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  message = 'Ćwiczenie od MMO';
+  buttonVisibility$: Observable<boolean>;
+  buttonVisibility: boolean = true;
+
+  constructor(private addToCartVisibiltyService: addToCartVisibiltyService) {}
+
+  ngOnInit(): void {
+    this.setupDataSources();
+  }
+
+  addToCart(): void {
+    this.fetchAddToCartVisibility();
+  }
+
+  private setupDataSources(): void {
+    this.buttonVisibility$ =
+      this.addToCartVisibiltyService.setAddToCartVisibility();
+  }
+
+  private fetchAddToCartVisibility(): void {
+    this.buttonVisibility$.subscribe((res) => {
+      this.buttonVisibility = res;
+    });
+  }
 }
